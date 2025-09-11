7574095dd71a784cea094f93ebf4805bf5dfa789
Yeferson Adrian Huarachi Aleman <roberthuarachi27@gmail.com>
2025-09-10 23:26:03 -0400

    Init Controller Monitoreo

--- src/modules/monitoreo/controllers/monitoreo.controller.ts
+++ src/modules/monitoreo/controllers/monitoreo.controller.ts
@@ -1,8 +1,39 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
+import { MonitoreoService } from "../services/monitoreo.service";
 
 
 @Controller('monitoreo')
 export class MonitoreoController {
+
+    constructor(private readonly monitoreoService: MonitoreoService) {}
+
+    @Get('riesgo-abandono')
+    async getPacientesEnRiesgo(
+       @Query('dias') dias: number // Días sin asistir (default: 7)
+    ) {
+        //return await this.monitoreoService.getPacientesEnRiesgoAbandonoTratamiento(dias);
+    }
+
+    @Get('abandonados')
+    async getPacientesAbandonados(
+         @Query('dias') dias: number // Días para considerar abandono (default: 30)
+    ){
+        //return await this.monitoreoService.getPacientesAbandonados(dias);
+    }
+
+    @Get('citas-perdidas')
+    async getCitasPerdidas(
+        @Query('fechaInicio') fechaInicio: string, // Fecha de inicio en formato ISO
+        @Query('fechaFin') fechaFin: string // Fecha de fin en formato ISO
+    ) {
+        //return await this.monitoreoService.getCitasPerdidas(fechaInicio, fechaFin);
+    }
+
+    @Get('adherencia/:idPaciente')
+    async getAdherenciaPaciente(@Query('idPaciente') idPaciente: string) {
+        //return await this.monitoreoService.getEstadisticasAdherencia(idPaciente);
+    }
+
 
 
 }
--- src/modules/monitoreo/services/monitoreo.service.ts
+++ src/modules/monitoreo/services/monitoreo.service.ts
@@ -1,5 +1,61 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Paciente } from "src/modules/paciente/entities/paciente.entity";
+import { Cita } from "src/modules/tratamiento/entities/cita.entity";
+import { TratamientoTB } from "src/modules/tratamiento/entities/tratamientoTB.entity";
+import { Repository } from "typeorm";
 
 
 @Injectable()
-export class MonitoreoService {}
+export class MonitoreoService {
+    constructor(
+        @InjectRepository(Paciente) private pacienteRepository: Repository<Paciente>,
+        @InjectRepository(Cita) private citaRepository: Repository<Cita>,
+        @InjectRepository(TratamientoTB) private tratamientoRepository: Repository<TratamientoTB>,
+    ) {}
+
+    // Pacientes en riesgo de abandono
+    async getPacientesEnRiesgoAbandonoTratamiento(diasSinAsistir: number){ // TODO: FALTA ANALIZAR
+        const fechaLimite = new Date();
+        fechaLimite.setDate(fechaLimite.getDate() - diasSinAsistir);
+        const pacientes = await this.pacienteRepository.createQueryBuilder('paciente')
+            .leftJoinAndSelect('paciente.tratamientos', 'tratamiento')
+            .leftJoinAndSelect('tratamiento.citas', 'cita')
+            .where('cita.fecha < :fechaLimite', { fechaLimite })
+            
+            .getMany();
+
+        return pacientes;
+    }
+
+    // Pacientes que han abandonado
+    async getPacientesAbandonados(diasAbandono: number) {  // TODO: Falta Analizar
+        const fechaLimite = new Date();
+        fechaLimite.setDate(fechaLimite.getDate() - diasAbandono);
+
+        return await this.tratamientoRepository
+            .createQueryBuilder('tratamiento')
+            .leftJoinAndSelect('tratamiento.paciente', 'paciente')
+            .leftJoinAndSelect('tratamiento.citas', 'cita')
+            .where('tratamiento.estado = :estado', { estado: 'Abandonado'})
+            .orWhere( qb => {
+                const subQuery = qb.subQuery()
+                    .select('MAX(cita.fecha_programada)', 'ultimaCita')
+                    .from('cita', 'cita')
+                    .where('cita.tratamientoId = tratamiento.id')
+                    .getQuery();
+                return `cita.fecha_programada IS NULL OR cita.fecha_programada < ${subQuery}`;
+            })
+    }
+
+    // Citas perdidas en un rango de fechas
+    async getCitasPerdidas(fechaInicio: Date, fechaFin: Date) {  // TODO: Falta Analizar
+        return await this.citaRepository.createQueryBuilder('cita')
+            .leftJoinAndSelect('cita.paciente', 'paciente')
+            .where('cita.fecha_programada BETWEEN :fechaInicio AND :fechaFin', { fechaInicio, fechaFin })
+            .andWhere('cita.estado = :estado', { estado: 'Perdida' })
+            .getMany();
+    }
+
+    
+}
